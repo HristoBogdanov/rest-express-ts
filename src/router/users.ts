6235362889f905e2c86dd0b498e11c2759db2c5d
Hristo Bogdanov <hbogdanov@elevatecompany.eu>
2024-10-03 22:44:52 +0300

    add update and delete actions and isOwner middleware

--- src/router/users.ts
+++ src/router/users.ts
@@ -1,5 +1,8 @@
-import { getAllUsers } from "../controllers/users";
+import { deleteUser, getAllUsers, updateUser } from "../controllers/users";
+import { isAuthenticated, isOwner } from "../middlewares";
 
 export default (router: any) => {
-  router.get("/users", getAllUsers);
+  router.get("/users", isAuthenticated, getAllUsers);
+  router.delete("/users/:id", isAuthenticated, isOwner, deleteUser);
+  router.patch("/users/:id", isAuthenticated, isOwner, updateUser);
 };
